3320e01c5240bcd8000d913a429b476a0b002b30
vincensiusadyatma <adyatmav@gmail.com>
2023-06-23 11:53:30 +0700

    update data barang

--- src/main/java/projectku/Gudang.java
+++ src/main/java/projectku/Gudang.java
@@ -8,6 +8,7 @@ public class Gudang {
     private ArrayList<Barang> wearable = new ArrayList<>();
 
     Gudang() {
+        //Kategori  Elektronik
         elektronik.add(new Barang("TCL 32 inch Smart LED TV", "TCL 32A7 SERIES, Best New Android TV. A+ Panel Quality, Feel the Comfortable\n" +
                 "TCL 32A7, Smart TV terjangkau yang bikin harimu semakin seru\n", 1849000));
         elektronik.add(new Barang("Toshiba LED TV - HD Smart TV 32 inch", "TOSHIBA LED TV - HD SMART VIDAA 32 inch adalah smart TV yang dilengkapi prosesor tercanggih \n" +
@@ -30,6 +31,27 @@ public class Gudang {
         elektronik.add(new Barang("HAN RIVER Mixer HRDDQ01WT", "Mixer Tangan HAN RIVER HE-133 membantu pekerjaan Anda mempermudah proses mixing makanan berjenis semi solid atau pun semi liquid,\n" +
                 "sehingga memasak atau membuat kue jadi pekerjaan yang menyenangkan." +
                 "\nDengan motor bertenaga 180 Watt yang tangguh, mixer ini hadir dengan pengocok telur", 65000));
+        elektronik.add(new Barang("TEKO LISTRIK SCARLETT 2 LITER","Material bahan dengan lapisan stainless anti karat yang sangat tahan lama dan aman untuk air mendidih. "
+                + "\nKetel hemat energi ini memiliki desain unik dengan penuangan sempurna sehingga sangat mudah digunakan, "
+                + "\nAnda dapat membuat secangkir minuman favorit Anda kapan pun Anda mau Kapasitas 2L yang cukup besar untuk Anda gunakan.",54000));
+        elektronik.add(new Barang("SETRIKA UAP PORTABLE","BISA BERPUTAR 180DERAJAT GAMPANG DIBAWA KEMANA-MANA GAMPANG DIPAKAI JIKA BARANG BERKENDALA WAJIB UNBOX,DAN JGAN KONFIM DLU",75738));
+        elektronik.add(new Barang("SHARP RICE COOKER / MAGIC COM KS-N18MG-RD(1,8 LITER)","SHARP KSN18MG Rice Cooker [1.8 Liter], rice cooker dengan desainnya yang modern dan elegan membuat nuansa di dapur anda semakin lebih stylish. "
+                + "\nDengan Anda pun dapat membuat kukusan kue serta menghangatkan makanan kembali dengan mudah.",189000));
+        elektronik.add(new Barang("Mesin Jahit Mini Portable (Plus Meja)","Mesin jahit mini yg efisien dan simple untuk menjahit baju, kain dan berbagai macam kerajinan tangan. Sangat membantu sekali, \ndengan cara penggunaan yang mudah dan praktis. "
+                + "\nTidak hanya untuk dewasa, buat anak-anak yang mau belajar menjahit pun bisa",134900));
+        elektronik.add(new Barang("TNW Car Vacum Cleaner Penyedot Debu 12000Pa","Penyedot debu mini kami adalah bagian tak terpisahkan dari rumah, mobil, dan kantor. Alat ini dapat membersihkan ceceran sampah di lemari, celah sofa, rel jendela, "
+                + "\nsisa cemilan, bulu hewan peliharaan, celah mobil, keyboard, dll. "
+                + "\nUntuk memberi Anda lingkungan rumah dan lingkungan kantor yang bersih dan rapi.",129000));
+        elektronik.add(new Barang("Stik PS3 / Stick PS3 PlaytSation 3 OP Freegun Edition Wireless","Stik Stick Playt-Sation 3 Wireles OP Getar Kualitas Terbaik, Harga Murah Tapi Tidak Murahan, Kualitas Dijamin, Bukan Stik Abal-abal Buktikan Sendiri Guyss !!! Tidak Bagus Kembalikan Saja",48900));
+        elektronik.add(new Barang("CCTV 3 Antena Wireless Smart Camera HD 1080P 2 MP Auto Tracking Camera Q5YS","~Auto screen shot saat terdeteksi gerakan yg tersimpan di Server bs di download ke App & Hp, bs dibuka meski camera offline/dirusakin maling, kl merk yg lain ga bisa "
+                + "\n~Rekaman bs lbh lama dibanding merk lain "
+                + "\n~Putar Horizontal 360, Vertikal 120 derajat "
+                + "\n~Cycle Recording: hapus & timpa otomatis dgn rekaman baru",135000));
+        elektronik.add(new Barang("RADIO AC/DC 4250 PORTABLE INTERNATIONAL 2 BAND FM - AM ANTIK CLASSIC JADUL","Radio ini dapat dioperasikan dengan kabel (termasuk dalam pembelian) sehingga Anda tak perlu repot-repot membeli baterai. "
+                + "\nNamun jika Anda ingin lebih mobile dengan radio ini, bisa juga dipasangi baterai.",69990));
+        
+        
+        //Kategori Fashion
         fashion.add(new Barang("Sepatu 910 Yuza matterHorn", "Sepatu lari berwarna hitam dengan paduan warna abu-abu", 476910));
         fashion.add(new Barang("Sepatu Compass Retrograde Black White", "BNIB Low Black White\n" +
                 "Original 100%\n" +
@@ -109,7 +131,58 @@ public class Gudang {
         fashion.add(new Barang("SSST Furii Denim Kimono Oversize Light Indigo S", "Furi Kimono adalah jaket denim yang terinspirasi dengan cutting noragi ala Jepang atau yang biasa disebut kimono." +
                 "\nDi desain open front dengan siluet drop shoulder menjadikan Kimono sempurna untuk menjadi layer luar outfitmu." +
                 "\nKimoni ini terbuat dari denim 12oz dan memiliki fitur empat saku dan aksen tali ikat di bagian depan.\n", 499000));
-
+        
+        
+        //Kategori Wearable
+        wearable.add(new Barang("Smartwatch Apple Watch Series 7","Tampil lebih stylish dengan Apple Watch Series 7! Smartwatch canggih dengan desain elegan dan fitur terbaru. Layar yang lebih besar, deteksi detak jantung, "
+                + "\nGPS terintegrasi dan kemampuan water-resistant membuat Apple Watch Series 7 menjadi pilihan sempurna untuk gaya hidup aktif Anda. "
+                + "\nNikmati kontrol penuh atas kesehatan, notifikasi penting, serta akses mudah ke aplikasi favorit Anda, semua dalam satu perangkat yang mewah.",6499000));
+        wearable.add(new Barang("Smartwatch Samsung Galaxy Watch 4","Hadir dengan teknologi terdepan, Samsung Galaxy Watch 4 adalah teman sempurna bagi gaya hidup aktif Anda. Dengan layar AMOLED yang cerah dan tahan air hingga 50 meter, "
+                + "\nAnda dapat memantau aktivitas olahraga, tidur, detak jantung, dan bahkan menjawab panggilan telepon langsung dari pergelangan tangan Anda. "
+                + "\nDapatkan akses cepat ke notifikasi, aplikasi, dan berbagai fitur pintar lainnya dengan Samsung Galaxy Watch 4.",3999000));
+        wearable.add(new Barang("Earbuds Apple AirPods Pro","Rasakan pengalaman mendengarkan musik yang luar biasa dengan Apple AirPods Pro! Earbuds berkualitas tinggi ini dilengkapi dengan fitur noise cancellation aktif yang memungkinkan "
+                + "\nAnda menikmati suara yang jernih dan detail tanpa gangguan dari lingkungan sekitar. Desain in-ear yang nyaman dan tahan air membuat AirPods Pro menjadi pasangan sempurna untuk kegiatan sehari-hari Anda. "
+                + "\nSegera nikmati kebebasan musik tanpa batas dengan AirPods Pro",2499000));
+        wearable.add(new Barang("Fitbit Versa 3 Smartwatch","Tingkatkan gaya hidup sehat dan aktif Anda dengan Fitbit Versa 3 Smartwatch. Smartwatch ini dilengkapi dengan monitor detak jantung, GPS terintegrasi, "
+                + "\nserta pelacakan tidur dan aktivitas yang canggih. Nikmati notifikasi cerdas, kontrol musik, dan akses ke aplikasi di pergelangan tangan Anda. "
+                + "\nDengan tampilan yang elegan dan tahan air, Fitbit Versa 3 adalah teman sejati untuk gaya hidup sehat Anda",3199000));
+        wearable.add(new Barang("Samsung Galaxy Buds Pro","Hadir dengan kualitas suara premium, Samsung Galaxy Buds Pro memberikan pengalaman mendengarkan musik yang tak tertandingi. "
+                + "\nFitur noise cancellation yang adaptif memungkinkan Anda fokus pada musik tanpa terganggu oleh suara di sekitar. "
+                + "\nDengan desain yang ergonomis dan tahan air, Galaxy Buds Pro memberikan kenyamanan maksimal sepanjang hari. "
+                + "\nJelajahi dunia suara yang kaya dan mendalam dengan Samsung Galaxy Buds Pro.",2299000));
+        wearable.add(new Barang("Garmin Forerunner 945 Smartwatch","Untuk pelari dan penggemar kebugaran serius, Garmin Forerunner 945 adalah pilihan yang tepat. Dengan pelacakan GPS yang akurat, "
+                + "\nmonitor detak jantung, dan analisis kebugaran yang mendalam, smartwatch ini membantu Anda mencapai performa terbaik Anda. "
+                + "\nFitur-fitur cerdas seperti notifikasi smartphone, pemantauan stres, dan pemulihan yang disesuaikan menjadikan "
+                + "\nForerunner 945 pendamping sempurna dalam setiap latihan",9999000));
+        wearable.add(new Barang("Xiaomi Mi Band 6","Xiaomi Mi Band 6 memberikan pemantauan kebugaran yang canggih dengan harga yang terjangkau. Layar AMOLED yang cerah dan warna-warni, "
+                + "\npelacakan tidur yang akurat, serta sensor detak jantung dan SpO2 membuat Mi Band 6 menjadi alat pelacak kesehatan yang hebat. "
+                + "\nTersedia berbagai mode olahraga dan notifikasi cerdas untuk membuat gaya hidup aktif Anda semakin menyenangkan.",499000));
+        wearable.add(new Barang("JBL Free X Earbuds","Dengarkan musik dengan kebebasan tanpa kabel dengan JBL Free X Earbuds! Earbuds ini hadir dengan suara yang kuat dan jernih, "
+                + "\nserta desain yang ergonomis untuk kenyamanan maksimal. Dengan baterai yang tahan lama dan fitur pengisian cepat, "
+                + "\nAnda dapat menikmati hingga 24 jam musik non-stop. JBL Free X Earbuds adalah teman sempurna untuk penggemar musik yang aktif.",1299000));
+        wearable.add(new Barang("Huawei Watch Fit Smartwatch","Dapatkan pengalaman smartwatch yang canggih dengan Huawei Watch Fit! Dengan tampilan AMOLED yang cerah dan pelacakan olahraga yang komprehensif, "
+                + "\nAnda dapat memantau dan meningkatkan kesehatan serta kebugaran Anda. Watch Fit dilengkapi dengan sensor detak jantung, pemantauan tidur, "
+                + "\ndan berbagai mode olahraga yang membantu Anda mencapai target fitness Anda. Nikmati notifikasi cerdas, musik, "
+                + "\ndan baterai tahan lama dalam satu perangkat yang ringkas.",1899000));
+        wearable.add(new Barang("Sony WF-1000XM4 True Wireless Earbuds","Rasakan suara premium tanpa batas dengan Sony WF-1000XM4 True Wireless Earbuds! Dilengkapi dengan teknologi noise cancellation yang adaptif, "
+                + "\nearbuds ini memungkinkan Anda menikmati suara yang jernih dan detail tanpa gangguan. Desain yang nyaman, "
+                + "\nbaterai tahan lama, dan fitur-fitur cerdas seperti pengenalan aktivitas membuat WF-1000XM4 menjadi pilihan "
+                + "\nterbaik untuk pengalaman mendengarkan musik yang luar biasa.",3899000));
+        wearable.add(new Barang("Fitbit Charge 4 Fitness Tracker","Dapatkan pelacak kebugaran yang andal dengan Fitbit Charge 4. Dilengkapi dengan monitor detak jantung, GPS terintegrasi, "
+                + "\ndan pelacakan aktivitas yang komprehensif, Fitbit Charge 4 membantu Anda mencapai tujuan kebugaran Anda."
+                + "\nLayar berwarna yang cerah, notifikasi smartphone, dan baterai tahan lama membuatnya menjadi teman setia sepanjang hari.",1799000));
+        wearable.add(new Barang("Bose Frames Audio Sunglasses","Gabungkan gaya dan teknologi dengan Bose Frames Audio Sunglasses. Kacamata hitam ini dilengkapi dengan speaker terintegrasi yang menghadirkan suara yang jernih dan imersif langsung ke telinga Anda. "
+                + "\nSelain melindungi mata dari sinar matahari, Anda juga dapat menikmati musik, panggilan telepon, dan asisten suara tanpa memerlukan headphone tambahan. "
+                + "\nTampil trendy sambil tetap terhubung dengan dunia di sekitar Anda.",3499000));
+        wearable.add(new Barang("Garmin Venu 2 Smartwatch","Dengan layar AMOLED yang cerah dan tahan air, Garmin Venu 2 Smartwatch memberikan kombinasi sempurna antara kecantikan dan kinerja."
+                + "\nDilengkapi dengan berbagai fitur kebugaran seperti pemantauan oksigen darah, pelacakan energi tubuh, dan ratusan mode olahraga, "
+                + "\nVenu 2 membantu Anda mencapai performa terbaik. Dapatkan juga akses ke notifikasi cerdas, aplikasi, dan pemutar musik langsung di pergelangan tangan Anda.",5999000));
+        wearable.add(new Barang("Sony WH-1000XM4 Wireless Noise-Canceling Headphones","Rasakan pengalaman mendengarkan musik tanpa batas dengan Sony WH-1000XM4. Headphone nirkabel ini dilengkapi dengan teknologi noise cancellation yang unggul, "
+                + "\nmenghilangkan suara bising di sekitar Anda untuk kualitas suara yang optimal. Kualitas suara yang mendalam, kenyamanan yang luar biasa, "
+                + "\ndan baterai tahan lama menjadikan WH-1000XM4 pilihan terbaik bagi penggemar musik yang menginginkan pengalaman dengar yang istimewa.",4999000));
+        wearable.add(new Barang("Xiaomi Amazfit GTS 2 Smartwatch","Dapatkan gaya dan kinerja dalam satu paket dengan Xiaomi Amazfit GTS 2 Smartwatch. Dilengkapi dengan layar AMOLED yang indah, pemantauan detak jantung, "
+                + "\ndan pelacakan tidur yang akurat, GTS 2 membantu Anda menjaga kesehatan dan kebugaran Anda. Fitur-fitur seperti notifikasi cerdas, "
+                + "\npemutar musik, dan tahan air hingga 50 meter menjadikannya pilihan sempurna untuk gaya hidup aktif Anda.",2099000));
     }
 
     public ArrayList<Barang> getElektronik() {
